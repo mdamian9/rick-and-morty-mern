8c9d5269cf1ab9c6dec9a6ee258cfbed00db605f
mdamian9 <m.damian9@hotmail.com>
2020-03-18 19:46:16 -0700

    Connected to MongoDB cloud, using express routing - created indexRouter

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+// Require Express, CORS, Mongoose, Morgan, and Path
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const path = require('path');
 
+// Require dotenv to load environment variables
 require('dotenv').config();
 
+// Initialize app and port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Use CORS middleware / setting CORS
 app.use(cors());
 
 // Use morgan to log all requests to the console
@@ -17,12 +22,24 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'GET / successful'
-    });
+// Initialize MongoDB URI and connect to database
+mongoose.connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+).catch(err => {
+    console.log(err);
+});
+mongoose.connection.once('open', () => {
+    console.log('>>> Connected to MongoDB successfully');
 });
 
+// Initialize API routes
+const indexRouter = require('./api/routes/index.router');
+
+// Use API routes
+app.use('/', indexRouter);
+
+// Start up server
 app.listen(PORT, () => {
     console.log(`>>> Server now on port ${PORT}`);
 });

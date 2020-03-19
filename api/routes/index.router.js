2d9d089e9885effe7359656249664be93a2a6545
mdamian9 <m.damian9@hotmail.com>
2020-03-18 21:35:30 -0700

    Completed /signup and /login routes, successfully tested using Postman

--- api/routes/index.router.js
+++ api/routes/index.router.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'GET / test route'
-    });
-});
+const jwt = require('jsonwebtoken');
+const db = require('../models');
 
 // Signup route
 router.post('/signup', (req, res, next) => {
-    
-})
+    db.User.create(req.body).then(() => {
+        res.status(201).json({ message: 'Successfully created new account' });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            message: 'Error occurred',
+            error: err
+        });
+    });
+});
 
 // Login route
-
-router.post('/')
+router.post('/login', (req, res, next) => {
+    db.User.findOne({ username: req.body.username }).then(user => {
+        user.verifyPassword(req.body.password, (err, isMatch) => {
+            if (!err && isMatch) {
+                let token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: 129600 });
+                res.json({ success: true, message: "Token issued!", token: token, user: user });
+            } else {
+                res.status(401).json({ success: false, message: "Authentication failed. Wrong password." });
+            };
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(404).json({
+            success: false,
+            message: 'User not found',
+            error: err
+        });
+    });
+});
 
 module.exports = router;

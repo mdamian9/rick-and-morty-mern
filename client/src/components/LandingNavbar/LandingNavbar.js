06b75f8a537d8f1fd33c364c444e3e2d63acb2e2
mdamian9 <m.damian9@hotmail.com>
2020-03-21 23:59:32 -0700

    Created LandingNavbar

---
+++ client/src/components/LandingNavbar/LandingNavbar.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, Navbar, NavbarBrand, Nav } from 'reactstrap';
+
+class LandingNavbar extends Component {
+
+    handleChange = event => {
+        // Extract name & value from event.target and set to state
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        // Extract email and password from event.target
+        const { email, password } = event.target;
+        console.log(`email: ${email.value}, password: ${password.value}`);
+        // Clear form fields
+        document.getElementById('login-form').reset();
+        // Reset state
+        this.setState({
+            email: '',
+            password: ''
+        });
+    };
+
+    render = () => {
+        return (
+            <Navbar color='light' light expand='md'>
+                <NavbarBrand href='/'>
+                    <Logo />
+                </NavbarBrand>
+                <Nav className='ml-auto' navbar>
+                    <Form inline id="login-form" onSubmit={this.handleFormSubmit}>
+                        <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
+                            <Label for='login-email' className='mr-sm-2'>Email:</Label>
+                            <Input type="email"
+                                name="email" id="login-email"
+                                placeholder="Enter your email"
+                                onChange={this.handleChange}
+                            />
+                        </FormGroup>
+                        <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
+                            <Label for='login-password' className='mr-sm-2'>Password:</Label>
+                            <Input type="password"
+                                name="password" id="login-password"
+                                placeholder="Enter your password"
+                                onChange={this.handleChange}
+                            />
+                        </FormGroup>
+                        <Button>Log In</Button>
+                    </Form>
+                </Nav>
+            </Navbar>
+        );
+    };
+
+};
+
+export default LandingNavbar;
